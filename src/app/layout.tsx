6c72a28e000fc660594787f7acea18407fff25ce
Irfan Hamid <irfansyukri597@gmail.com>
2025-08-23 18:51:43 +0700

    fix: update toastify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppLayout from "@/components/partials/app-layout";
-import "react-toastify/dist/ReactToastify.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
