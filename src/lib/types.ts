86e88411665f804509f5bdfe2ab7d891814db7e2
Alvin Miftah <azraqazravin@gmail.com>
2025-12-03 10:41:45 +0700

    feat: update job level CRUD

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -56,6 +56,7 @@ export interface IEmployee {
 export interface IDepartment {
   id: number;
   name: string;
+  level: number;
   description?: string | null;
   created_at: string;
   updated_at: string;
